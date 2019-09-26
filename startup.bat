24ee01e133aed96862a68c9dbc471179bc607c9b
HarshShah24 <hshsharshshah197@gmail.com>
2019-09-26 23:36:09 +0530

    Base Version
    Contains path to exe's which on invoking .bat file will open all the applications

---
+++ startup.bat
@@ -0,0 +1,19 @@
+start "C:\Users\harsshah\Downloads\Softwares\MobaXterm_Personal_10.9"
+
+start /d "C:\Program Files (x86)\XML Notepad 2007\" XmlNotepad
+
+start /d "C:\Program Files (x86)\Google\Chrome\Application\" chrome
+
+start /d "C:\Program Files\FileZilla FTP Client\" filezilla
+
+start /d "C:\Program Files\Perforce\" p4V
+
+start /d "C:\Users\harsshah\Downloads\Softwares\Development\VFIT\sqldeveloper\" sqldeveloper
+
+start /d "C:\Users\harsshah\Downloads\Softwares\Development\VFIT\eclipseOxy\" eclipse
+
+start /d "C:\Program Files\internet explorer"  iexplore
+
+start /d "C:\Program Files\Notepad++\" notepad++
+
+@pause
